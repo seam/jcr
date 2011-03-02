7a3de86edfd94295920841463643653e5561e983
George Gastaldi <gastaldi@apache.org>
2011-03-01 23:25:01 -0300

    Improving javadocs

--- impl/src/main/java/org/jboss/seam/jcr/JcrExtension.java
+++ impl/src/main/java/org/jboss/seam/jcr/JcrExtension.java
@@ -19,10 +19,12 @@ package org.jboss.seam.jcr;
 
 import javax.enterprise.inject.spi.Extension;
 
-/**  I doubt this extension will be necessary, at least until we start more bootstrapping.
- *
+/**
+ * I doubt this extension will be necessary, at least until we start more
+ * bootstrapping.
+ * 
  * @author johnament
  */
-public class JcrExtension implements Extension{
+public class JcrExtension implements Extension {
 
 }
